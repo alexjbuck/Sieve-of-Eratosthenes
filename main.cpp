87269da74791ded9140fdfdab7042d321b24a65f
alexjbuck <alexjbuck@gmail.com>
2014-03-25 19:06:36 -0500

    upgraded sieve by not indexing even numbers to save memory / allow for finding larger primes

--- main.cpp
+++ main.cpp
@@ -2,38 +2,44 @@
 #include <iostream>
 #include <time.h>
 
-#define nPrimes 50000000
+#define nArray 50000000
 
 int main() {
-	std::bitset<nPrimes> primeArray;
+	std::bitset<nArray> primeArray; // Only odd numbers since we know all evens are non-prime (Except 2)
 	clock_t t1,t2;
 	t1 = clock();
 
 	// Set all to 1 (possibly prime)
-	primeArray.set(); 
-	// Numbers 0 and 1 are not prime
-	primeArray.reset(0);
-	primeArray.reset(1);
+	primeArray.set();
+	primeArray.reset(0); // 1 is not prime.
+	// Array looks like
+	// 1 3 5 7 9 11 13 15 etc...
+	// 0 1 2 3 4 5  6  7
 	
 	int primeIndex = 0;
+	int prime;
 	int multipleIndex;
 	std::cout << "Searching for primes..." << std::endl;
-	// Iterate through all numbers between 2 and nPrimes
-	while(primeIndex<nPrimes) {
+	// Iterate through all numbers between 2 and nArray
+	while(primeIndex<nArray) {
 		// Advance primeIndex until it finds the next "non-multiple" number
 		primeIndex++;
-		if(primeArray[primeIndex]) {		
-			std::cout << primeIndex << " ";
+		if(primeArray[primeIndex]) {
+			prime = 2*primeIndex + 1;
+#ifndef NO_OUTPUT
+			std::cout << prime << " ";
+#endif
 			// Clear all multiples of primes
-			multipleIndex = primeIndex * 2;
-			while(multipleIndex<nPrimes) {
+			multipleIndex = primeIndex + prime ;
+			while(multipleIndex<nArray) {
 				primeArray.reset(multipleIndex);
-				multipleIndex += primeIndex;
+				multipleIndex += prime;
 			}
 		}
 	}
 	std::cout << std::endl;
 	t2 = clock();
-	std::cout << "Found " << primeArray.count() << " primes between 2 and " << nPrimes << std::endl;
-	std::cout << "Total elapsed time: " << ((float)(t2-t1))/CLOCKS_PER_SEC << " Average " << ( ((float)(t2-t1)/CLOCKS_PER_SEC)/primeArray.count()) << " seconds per prime." << std::endl;
+	int primesFound = primeArray.count() + 1; // +1 for 2 being prime
+	std::cout << "Found " << primesFound << " primes between 2 and " << 2*nArray+1 << std::endl;
+	std::cout << "Total elapsed time: " << ((float)(t2-t1))/CLOCKS_PER_SEC << " Average " << ( ((float)(t2-t1)/CLOCKS_PER_SEC)/primesFound) << " seconds per prime." << std::endl;
 }
---
+++ makefile
@@ -0,0 +1,11 @@
+all:
+	g++ -o sieve main.cpp
+
+opt:
+	g++ -o sieve_opt main.cpp -O3
+
+quiet:
+	g++ -o sieve_quiet main.cpp -DNO_OUTPUT
+
+quiet_opt:
+	g++ -o sieve_quiet_opt main.cpp -O3 -DNO_OUTPUT
